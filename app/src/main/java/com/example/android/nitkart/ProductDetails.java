a5f4842c49961808b3636b5aec3ffdb1ec87baef
suyash0103 <suyashghuge@gmail.com>
2018-03-13 22:08:24 +0530

    delete half done

--- app/src/main/java/com/example/android/nitkart/ProductDetails.java
+++ app/src/main/java/com/example/android/nitkart/ProductDetails.java
@@ -2,17 +2,35 @@ package com.example.android.nitkart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ProductDetails extends AppCompatActivity {
 
+    Button delete;
     ImageView imageView;
     TextView producName, productPrice, productId, seller_name, seller_phone, seller_email, seller_block, seller_room, time_period;
+    String prod_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +48,7 @@ public class ProductDetails extends AppCompatActivity {
         seller_room = (TextView) findViewById(R.id.seller_room);
         time_period = (TextView) findViewById(R.id.time_period);
 
+        delete = (Button) findViewById(R.id.delete);
 
         Album album = (Album) getIntent().getSerializableExtra("Album");
         Picasso.with(this).load(MainActivity.domain + album.getUrl()).into(imageView);
@@ -42,5 +61,40 @@ public class ProductDetails extends AppCompatActivity {
         seller_block.setText(album.getSeller_block());
         seller_room.setText(album.getSeller_room());
         time_period.setText(album.getTime_period());
+
+        prod_id = album.getProduct_id();
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.DELETE, MainActivity.domain + "/user/delete/",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(ProductDetails.this, "Ad Deleted", Toast.LENGTH_SHORT).show();
+                                Log.d("Response", response);
+                                finish();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                //this is causing an error
+//                            Toast.makeText(getContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                ) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("id", prod_id);
+                        return params;
+                    }
+                };
+                SingletonRequestQueue.getInstance(ProductDetails.this).addToRequestQueue(stringRequest);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/nitkart/ProfileFragment.java
+++ app/src/main/java/com/example/android/nitkart/ProfileFragment.java
@@ -335,4 +335,5 @@ public class ProfileFragment extends Fragment {
     }
 
 
+
 }
