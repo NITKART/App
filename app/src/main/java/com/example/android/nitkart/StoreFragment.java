2eaf78454d3d83f6e784dbc504567a0110402448
suyash0103 <suyashghuge@gmail.com>
2018-03-13 23:31:13 +0530

    swipe refresh fixed

--- app/src/main/java/com/example/android/nitkart/StoreFragment.java
+++ app/src/main/java/com/example/android/nitkart/StoreFragment.java
@@ -109,10 +109,9 @@ public class StoreFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_store, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         swipeRefreshLayout = view.findViewById(R.id.swipeRefresh);
-	swipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
-	            android.R.color.holo_green_light, 
-	            android.R.color.holo_orange_light, 
-	            android.R.color.holo_red_light);
+        swipeRefreshLayout.setColorScheme(android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
         albumList = new ArrayList<>();
         images = new ArrayList<>();
@@ -353,6 +352,7 @@ public class StoreFragment extends Fragment {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                        swipeRefreshLayout.setRefreshing(false);
 //                        Intent intent = new Intent(getContext(), NoInternetActivity.class);
 //                        startActivity(intent);
                     }
